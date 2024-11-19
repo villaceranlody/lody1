08c95010a0caf846e637de04f5fea94c3c9e39db
villaceranlody <146922763+villaceranlody@users.noreply.github.com>
2024-11-19 14:37:51 +0800

    config.php

--- lody1/config.php
+++ lody1/config.php
@@ -2,8 +2,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'lody');
+define('DB_PASSWORD', 'villaceran');
 define('DB_NAME', 'demo');
  
 /* Attempt to connect to MySQL database */
